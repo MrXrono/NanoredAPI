670dba125562182a0f9dda54cfdeb0364cce785d
automation <automation@local>
2026-02-18 20:24:26 +0000

    Bump version 1.7.0.1

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     PROJECT_NAME: str = "NanoredVPN API"
-    VERSION: str = "1.7.0.0"
+    VERSION: str = "1.7.0.1"
     API_V1_PREFIX: str = "/api/v1"
 
     # Database
